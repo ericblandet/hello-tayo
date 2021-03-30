6194283ddb729e11c4eac512073bcf7063948cb0
Eric blandet <ericblandet@Erics-MacBook-Pro.local>
2021-03-30 08:26:44 +0200

    component created

---
+++ src/components/HelloTayo.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>🙂 Click to toggle color 🙃</h2>
+</template>
+
+<script>
+export default {
+  name: "HelloTayo",
+  setup() {},
+};
+</script>
+
+<style scoped>
+h2 {
+  background-color: #ffcc00;
+  padding: 10px 15px;
+  display: inline-block;
+  border-radius: 2px;
+}
+</style>
